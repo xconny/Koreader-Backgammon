4ec29bce828e6cba6adf79cf5f3eb79729425df2
Chirheaux <37458780+xconny@users.noreply.github.com>
2025-12-29 11:28:15 +0100

    Create _meta.lua

---
+++ backgammon.koplugin/_meta.lua
@@ -0,0 +1,9 @@
+local _ = require("gettext")
+
+return {
+    name = "backgammon",
+    fullname = _([[Backgammon (Tavli)]]),
+    description = _([[Backgammon-style tables plugin for KOReader focusing first on the Greek variant Plakoto. Includes a mode selection screen and a basic 2-player Plakoto implementation with pinning instead of hitting.]]),
+    version = "0.1.0",
+    author = "chirheaux",
+}
